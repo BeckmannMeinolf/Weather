a21a662571b0a7ce82eddab0038e60784b8126f2
BeckmannMeinolf <meinolf.beckmann@bogensportler.de>
2018-02-28 01:33:12 +0100

    Initial commit

---
+++ Weather/Weather/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Weather
+//
+//  Created by Meinolf Beckmann on 28.02.18.
+//  Copyright © 2018 Meinolf Beckmann. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Weather/Weather/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by Meinolf Beckmann on 28.02.18.
+//  Copyright © 2018 Meinolf Beckmann. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
